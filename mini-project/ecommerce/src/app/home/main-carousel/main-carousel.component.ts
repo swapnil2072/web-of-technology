ddfe09a1cf55e5585a0e4dd9f1d0d44e54aebc49
swapnil2072 <swapnilshinde909693@gmail.com>
2024-03-01 23:38:15 +0530

    starting with main carousel component

---
+++ mini-project/ecommerce/src/app/home/main-carousel/main-carousel.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { homeCarouselData } from 'src/Data/mainCarousel';
+
+@Component({
+  selector: 'app-main-carousel',
+  templateUrl: './main-carousel.component.html',
+  styleUrls: ['./main-carousel.component.scss'],
+})
+export class MainCarouselComponent {
+  carouselData: any;
+
+  currentSlide = 0;
+  interval: any;
+
+  ngOnInit() {
+    this.carouselData = homeCarouselData;
+  }
+
+  autoPlay() {
+    setInterval(() => {
+      this.nextSlide();
+    }, 2000);
+  }
+
+  nextSlide() {
+    this.currentSlide = (this.currentSlide + 1) % this.carouselData.length;
+  }
+}
