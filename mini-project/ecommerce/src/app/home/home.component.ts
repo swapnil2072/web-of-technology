26ccb651d61d491479382988a1afb18cf9b3b816
swapnil2072 <swapnilshinde909693@gmail.com>
2024-03-03 19:06:31 +0530

    added product card

--- mini-project/ecommerce/src/app/home/home.component.ts
+++ mini-project/ecommerce/src/app/home/home.component.ts
@@ -1,10 +1,14 @@
+import { menJeans } from './../../Data/Men/men_jeans';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-
+  menJeans: any;
+  ngOnInit(){
+    this.menJeans = menJeans.slice(0,5)
+  }
 }
