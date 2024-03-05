2832289cd94ad622b69373597a4f16c2ce7094d8
swapnil2072 <swapnilshinde909693@gmail.com>
2024-03-05 23:02:00 +0530

    commit

--- mini-project/ecommerce/src/app/home/home.component.ts
+++ mini-project/ecommerce/src/app/home/home.component.ts
@@ -2,6 +2,8 @@ import { menJeans } from './../../Data/Men/men_jeans';
 import { Component } from '@angular/core';
 import { gounsPage1 } from 'src/Data/Gouns/gouns';
 import { lehngaCholiPage2 } from 'src/Data/Saree/lenghaCholiPage2';
+import { mensShoesPage1 } from '../../Data/shoes';
+import { kurtaPage1 } from 'src/Data/Kurta/kurta';
 
 @Component({
   selector: 'app-home',
@@ -19,5 +21,7 @@ export class HomeComponent {
     this.menJeans = menJeans.slice(0, 5);
     this.womenGouns = gounsPage1.slice(0, 5);
     this.lenghaCholi = lehngaCholiPage2.slice(0, 5);
+    this.menJeans = mensShoesPage1.slice(0, 5);
+    this.mensKurta = kurtaPage1.slice(0, 5);
   }
 }
